384a6362e46aeaf23f83e618702c56a2a0b9d595
chandu078 <chandudyavanapelli03@gmail.com>
2022-01-02 04:51:34 +0000

    sm7250-common: rro_overlays: Add RRO for OnePlusCamera & OnePlusGallery
    Signed-off-by: firebird11 <hbgassel@gmail.com>
    Change-Id: If69bfa3d417207b36bf08a0e87f8ab97916f0fbe
    Signed-off-by: c79 <vexed@riseup.net>
    Signed-off-by: aswin7469 <aswinas@pixysos.com>
    Signed-off-by: Sandeep-FED <sandymankara11@gmail.com>

--- common.mk
+++ common.mk
@@ -376,6 +376,18 @@ PRODUCT_PACKAGES += \
     libOmxVenc \
     libstagefrighthw
 
+# OnePlus
+#PRODUCT_PACKAGES += \
+#    oneplus-fwk
+#
+#PRODUCT_BOOT_JARS += \
+#    oneplus-fwk
+
+# OnePlus Apps
+PRODUCT_PACKAGES += \
+    OnePlusCameraOverlay \
+    OnePlusGalleryOverlay
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
