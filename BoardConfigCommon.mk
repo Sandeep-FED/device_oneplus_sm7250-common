6f7f86406cec882eeec0ac69b78eec629cd35fcb
Keertesh <keertesh.k98@gmail.com>
2022-03-02 17:28:16 +0000

    sm7250-common: Ignore neverallows

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -179,6 +179,7 @@ include device/qcom/sepolicy_vndr/SEPolicy.mk
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
+SELINUX_IGNORE_NEVERALLOWS := true
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
