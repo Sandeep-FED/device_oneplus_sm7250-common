bbb1e92ac601545ff817827120664f1d185b417d
Sandeep-FED <sandymankara11@gmail.com>
2022-04-24 09:48:05 +0000

    sm7250-common: exclude WFDCommon
    Signed-off-by: Sandeep-FED <sandymankara11@gmail.com>

--- common.mk
+++ common.mk
@@ -489,7 +489,7 @@ PRODUCT_PACKAGES += \
     libwfdaac_vendor
 
 PRODUCT_BOOT_JARS += \
-    WfdCommon
+   #WfdCommon
 
 PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
