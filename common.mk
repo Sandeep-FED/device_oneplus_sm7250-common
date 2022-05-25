6033b6561465ffd424caded1e8e7d1f8bc1d5a9e
aswin7469 <aswinas@pixysos.com>
2022-05-25 16:33:56 +0530

    sm7250-common: remove GCamGO
    This reverts commit b5ab9eb2478b1586866f4cb6a5c045f6f117a439.

--- common.mk
+++ common.mk
@@ -200,9 +200,6 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
-PRODUCT_PACKAGES += \
-    GCamGOPrebuilt
-
 # Common init scripts
 PRODUCT_PACKAGES += \
     init.class_main.sh \
