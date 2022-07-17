53f6c5e0adc682a7443dea9fd77883553bc25454
Sandeep-FED <sandymankara11@gmail.com>
2022-07-17 07:10:39 +0000

    sm7250-common: Boardconfig device fwk to lineage
    Signed-off-by: Sandeep-FED <sandymankara11@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -123,7 +123,7 @@ TARGET_SURFACEFLINGER_UDFPS_LIB := //hardware/oneplus:libudfps_extension.oneplus
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(COMMON_PATH)/device_framework_matrix.xml \
-    vendor/cherish/config/device_framework_matrix.xml
+    vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 
